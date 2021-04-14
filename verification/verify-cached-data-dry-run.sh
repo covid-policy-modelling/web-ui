1045f00dcd4f3ddb903dafaf995e539fc3b517f5
Steven Carlysle-Davies <S.Carlysle-Davies@epcc.ed.ac.uk>
2021-04-14 10:58:32 +0100

    Update verification data files

--- verification/verify-cached-data-dry-run.sh
+++ verification/verify-cached-data-dry-run.sh
@@ -11,7 +11,9 @@ CACHE=$CACHE/cache
 docker-compose exec web script/fetch-recorded-data --dry-run --cache-dir $CACHE
 
 docker-compose exec web cat $CACHE/case-data.json > $DATA_DIR/cases.actual.json
+npx prettier --write $DATA_DIR/cases.actual.json
 diff -q $DATA_DIR/cases.expected.json $DATA_DIR/cases.actual.json
 
 docker-compose exec web cat $CACHE/intervention-data.json > $DATA_DIR/interventions.actual.json
+npx prettier --write $DATA_DIR/interventions.actual.json
 diff -q $DATA_DIR/interventions.expected.json $DATA_DIR/interventions.actual.json
