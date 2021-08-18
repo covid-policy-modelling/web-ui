894f7a11d501097732a366f4e4fb2e1379c5e80e
Mario Antonioletti <mario@epcc.ed.ac.uk>
2021-08-18 16:01:06 +0100

    Fixed SQL silly cut-and-paste error.

--- migrations/sqls/20210331085844-insert-uk-subregions-up.sql
+++ migrations/sqls/20210331085844-insert-uk-subregions-up.sql
@@ -3,11 +3,11 @@ INSERT INTO regions (id, parent_id, name, created_at, updated_at) VALUES
   ('GB-ENG', 'GB', 'England', NOW(), NOW()),
   ('GB-NIR', 'GB', 'Northern Ireland', NOW(), NOW()),
   ('GB-SCT', 'GB', 'Scotland', NOW(), NOW()),
-  ('GB-WLS', 'GB', 'Wales', NOW(), NOW());
-  ('GB-LND', 'GB', 'London', NOW(), NOW());
-  ('GB-SW', 'GB', 'South West', NOW(), NOW());
-  ('GB-SE', 'GB', 'South East', NOW(), NOW());
-  ('GB-EE', 'GB', 'East of England', NOW(), NOW());
-  ('GB-MDL', 'GB', 'Midlands', NOW(), NOW());
-  ('GB-NE', 'GB', 'North East and Yorkshire', NOW(), NOW());
+  ('GB-WLS', 'GB', 'Wales', NOW(), NOW()),
+  ('GB-LND', 'GB', 'London', NOW(), NOW()),
+  ('GB-SW', 'GB', 'South West', NOW(), NOW()),
+  ('GB-SE', 'GB', 'South East', NOW(), NOW()),
+  ('GB-EE', 'GB', 'East of England', NOW(), NOW()),
+  ('GB-MDL', 'GB', 'Midlands', NOW(), NOW()),
+  ('GB-NE', 'GB', 'North East and Yorkshire', NOW(), NOW()),
   ('GB-NW', 'GB', 'North West', NOW(), NOW());
