b6bc413469630156ca4e25d0f7c1b507b6c71958
Steven Carlysle-Davies <S.Carlysle-Davies@epcc.ed.ac.uk>
2022-05-03 14:35:57 +0100

    Add /simulations/{id} endpoint

--- pages/api/simulations/[id]/index.ts
+++ pages/api/simulations/[id]/index.ts
@@ -2,43 +2,74 @@ import assert from 'assert'
 import {NextApiRequest} from 'next'
 import 'source-map-support/register'
 import {assertEnv} from '../../../../lib/assertions'
-import {updateSimulation} from '../../../../lib/db'
+import {getSimulation, updateSimulation} from '../../../../lib/db'
 import {catchUnhandledErrors} from '../../../../lib/handle-error'
 import {withDB} from '../../../../lib/mysql'
+import {Session} from '../../../../lib/session'
 import {RunStatus} from '../../../../lib/simulation-types'
 import dispatch from '../../util/dispatch'
+import requireSession from '../../util/require-session'
 
 catchUnhandledErrors()
 
 const RUNNER_SHARED_SECRET = assertEnv('RUNNER_SHARED_SECRET', true)
 
 export default withDB(conn =>
-  dispatch('POST', async (req, res) => {
-    if (!validateRequest(req)) {
-      res.status(401).json({error: 'Unauthorized'})
-      return
-    }
+  dispatch({
+    get: requireSession((session: Session) => async (req, res) => {
+      /*
+       * @oas [get] /simulations/{id}
+       * description: Retrieves simulation summary
+       * parameters:
+       *   - (path) id=84* {integer} Simulation ID
+       * responses:
+       *   200:
+       *    description: Successful operation
+       *    content:
+       *      application/json; charset=utf-8:
+       *        schema:
+       *          $ref: "#/components/schemas/Simulation"
+       * operationId: getSimulation
+       * tags: ["simulations"]
+       */
+      const sim = await getSimulation(conn, session.user, {
+        id: parseInt(req.query.id as string)
+      })
 
-    try {
-      verifyStatus(req.body.status)
-
-      const updated = await updateSimulation(
-        conn,
-        req.query.id as string,
-        req.body.status,
-        req.body.modelSlug,
-        req.body.resultsLocation,
-        req.body.exportLocation,
-        req.body.workflowRunID
-      )
-
-      if (updated) {
-        res.status(204).end()
-      } else {
-        res.status(404).end('Not found')
+      if (!sim) {
+        res.status(404).json({error: 'Not found'})
+        return
       }
-    } catch (e) {
-      res.status(400).json({error: `Could not process message: ${e.message}`})
+
+      res.status(200).json(sim)
+    }),
+    post: async (req, res) => {
+      if (!validateRequest(req)) {
+        res.status(401).json({error: 'Unauthorized'})
+        return
+      }
+
+      try {
+        verifyStatus(req.body.status)
+
+        const updated = await updateSimulation(
+          conn,
+          req.query.id as string,
+          req.body.status,
+          req.body.modelSlug,
+          req.body.resultsLocation,
+          req.body.exportLocation,
+          req.body.workflowRunID
+        )
+
+        if (updated) {
+          res.status(204).end()
+        } else {
+          res.status(404).end('Not found')
+        }
+      } catch (e) {
+        res.status(400).json({error: `Could not process message: ${e.message}`})
+      }
     }
   })
 )
